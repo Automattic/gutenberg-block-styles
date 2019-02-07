00911e1b6c61b4185205775cbc3d5ccf0226cdf7
Kjell Reigstad <kjell@kjellr.com>
2019-02-07 13:38:06 -0500

    Adds a simple music-theme-style Cover Block style.

--- block-styles.js
+++ block-styles.js
@@ -3,7 +3,7 @@
  * Duplicate and customize the four lines below for each style variation you'd like to add.
  */
 
-wp.blocks.registerBlockStyle( 'core/paragraph', {
-	name: 'blue-paragraph',
-	label: 'Blue Paragraph'
+wp.blocks.registerBlockStyle( 'core/cover', {
+	name: 'irregular-cover',
+	label: 'Irregular'
 } );
