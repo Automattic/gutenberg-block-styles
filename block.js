38ef0a7bbc2c77ea7255dd116b3988615e20e080
Kjell Reigstad <kjell@kjellr.com>
2019-07-09 13:03:53 -0400

    Add svg masks for the cover block.

--- block.js
+++ block.js
@@ -3,7 +3,22 @@
  * Duplicate and customize the four lines below for each style variation you'd like to add.
  */
 
-wp.blocks.registerBlockStyle( 'core/paragraph', {
-	name: 'blue-paragraph',
-	label: 'Blue Paragraph'
+wp.blocks.registerBlockStyle( 'core/cover', {
+	name: 'circle',
+	label: 'Circle Mask'
+} );
+
+wp.blocks.registerBlockStyle( 'core/cover', {
+	name: 'star',
+	label: 'Star Mask'
+} );
+
+wp.blocks.registerBlockStyle( 'core/cover', {
+	name: 'rounded-corners',
+	label: 'Rounded Corners'
+} );
+
+wp.blocks.registerBlockStyle( 'core/cover', {
+	name: 'bottom-left-round',
+	label: 'Bottom Left Round'
 } );
